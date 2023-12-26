7ff68c84a9d9ead06ae5aed7964545d31c050b77
Alex <naruto.xd1@mail.ru>
2023-12-27 00:19:17 +0200

    Добавлен код программы

---
+++ Program/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<string> FilterStrings(string[] arr)
+    {
+        List<string> result = new List<string>();
+        foreach (string s in arr)
+        {
+            if (s.Length <= 3)
+            {
+                result.Add(s);
+            }
+        }
+        return result;
+    }
+
+    static void Main()
+    {
+        // Введите массив строк с клавиатуры
+        Console.Write("Введите строки через запятую: ");
+        string inputStrings = Console.ReadLine();
+        string[] originalArray = inputStrings.Split(", ");
+
+        // Фильтрация строк
+        List<string> filteredArray = FilterStrings(originalArray);
+
+        // Вывод результата
+        Console.WriteLine("Исходный массив: " + string.Join(", ", originalArray));
+        Console.WriteLine("Отфильтрованный массив: " + string.Join(", ", filteredArray));
+    }
+}
